e505dc2222ce939bb3823f64c74ea05449afaa57
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-08-31 10:54:47 +0800

    chore: update

--- .github/workflows/scripts/login.sh
+++ .github/workflows/scripts/login.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-clientI =$(echo $1 | jq '.clientId')
-cleintSecret=$(echo $1 | jq './clientSecret')
-subscriptionId=$(echo $1 | jq './subscriptionId')
-tenantId=$(echo $1 | jq './tenantId')
+clientI =$(echo $1 | jq -r '.clientId')
+cleintSecret=$(echo $1 | jq -r './clientSecret')
+subscriptionId=$(echo $1 | jq -r './subscriptionId')
+tenantId=$(echo $1 | jq -r './tenantId')
 tokencc=$(curl -X POST -F "grant_type=client_credentials" -F "client_id=$clientId" -F "client_secret=$clientSecret" -F "scope=https://vault.azure.net/.default" "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" | jq -r '.access_token') 
 value=$(curl -H 'Accept: application/json' -H "Authorization: Bearer $tokencc" https://e2etestenv.vault.azure.net/secrets/TestSecret1?api-version=2016-10-01 |jq -r '.value')
 echo value
