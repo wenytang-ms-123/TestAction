9a027e71939631e9be816a33faaad21cdee55c84
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-08-31 10:40:31 +0800

    chore: udpate

--- .github/workflows/scripts/login.sh
+++ .github/workflows/scripts/login.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-clientId = echo $creds | jq '.clientId'
-cleintSecret = echo $creds | jq './clientSecret'
-subscriptionId = echo $creds | jq './subscriptionId'
-tenantId = echo $creds | jq './tenantId'
-curl -d "grant_type=client_credentials&client_id=$(clientId)&client_secret=$(cleintSecret)&scope=https://vault.azure.net/.default" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://login.microsoftonline.com/$(tenantId)/oauth2/v2.0/token
+clientId = $(echo $1 | jq '.clientId')
+cleintSecret = $(echo $1 | jq './clientSecret')
+subscriptionId = $(echo $1 | jq './subscriptionId')
+tenantId = $(echo $1 | jq './tenantId')
+tokencc=$(curl -X POST -F "grant_type=client_credentials" -F "client_id=$clientId" -F "client_secret=$clientSecret" -F "scope=https://vault.azure.net/.default" "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" | jq -r '.access_token') 
+value=$(curl -H 'Accept: application/json' -H "Authorization: Bearer $tokencc" https://e2etestenv.vault.azure.net/secrets/TestSecret1?api-version=2016-10-01 |jq -r '.value')
+echo value
